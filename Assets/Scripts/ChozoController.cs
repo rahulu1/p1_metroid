e76fa3fd998ff6463f8ecba9f258bacd6c814d01
Rahul Unniyampath <rahulu@umich.edu>
2023-09-17 15:16:52 -0400

    Add ChozoController

---
+++ Assets/Scripts/ChozoController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ChozoController : MonoBehaviour
+{
+    public float timeToClose = 6.5f;
+
+    private Animator chozoAnimator;
+
+    [SerializeField]
+    private GameObject ChozoPickup;
+    private void Start()
+    {
+        chozoAnimator = GetComponent<Animator>();
+    }
+    public void ProjectileEnter()
+    {
+
+        chozoAnimator.SetTrigger("Open");
+
+        StartCoroutine(CloseAfterTime(timeToClose));
+    }
+
+    void EnablePickupIfExists()
+    {
+        if (ChozoPickup != null)
+            ChozoPickup.SetActive(true);
+    }
+
+    IEnumerator CloseAfterTime(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        if(ChozoPickup != null)
+            ChozoPickup.SetActive(false);
+
+        chozoAnimator.SetTrigger("Close");
+
+    }
+}
