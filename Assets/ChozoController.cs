8c53d5968de186d31f8d9038d42105ce700550af
Rahul Unniyampath <rahulu@umich.edu>
2023-09-17 13:57:51 -0400

    P1-24 set up pickups around the map #time 30m

---
+++ Assets/ChozoController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ChozoController : MonoBehaviour
+{
+    public float timeToClose = 6.5f;
+
+    private Animator chozoAnimator;
+
+    [SerializeField]
+    private GameObject ChozoPickup;
+    private void Start()
+    {
+        chozoAnimator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            chozoAnimator.SetTrigger("Open");
+
+            StartCoroutine(CloseAfterTime(timeToClose));
+        }
+    }
+
+    void EnablePickupIfExists()
+    {
+        if (ChozoPickup != null)
+            ChozoPickup.SetActive(true);
+    }
+
+    IEnumerator CloseAfterTime(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        if(ChozoPickup != null)
+            ChozoPickup.SetActive(false);
+        chozoAnimator.SetTrigger("Close");
+
+    }
+}
