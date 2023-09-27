cb4dd07a34f03ce04a4b57e01e538343a5320a50
johnrose <johnrose@umich.edu>
2023-09-27 03:10:40 -0400

    Finished p1_gold

--- Assets/HintImage.cs
+++ Assets/HintImage.cs
@@ -6,6 +6,19 @@ using UnityEngine.UI;
 
 public class HintImage : HintText
 {
+    private bool won = false;
+    void Update()
+    {
+        if (!won)
+        {
+            if (unlocked)
+            {
+                won = true;
+                StartCoroutine(ShowHint());
+            }
+        }
+    }
+
     protected override IEnumerator ShowHint()
     {
         GetComponent<Image>().enabled = true;
--- Assets/HintText.cs
+++ Assets/HintText.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class HintText : MonoBehaviour
+public class HintText : Lockable
 {
     protected int timesCharged = 0;
     protected int hintEvery = 2;
+
+    private bool won = false;
     // Start is called before the first frame update
     protected void Start()
     {
         Chargeable.on_charge += DisplayHint;
+    }
+
+    void Update()
+    {
+        if (!won)
+        {
+            if (unlocked)
+            {
+                won = true;
+                TextMeshProUGUI tmpPro = GetComponent<TextMeshProUGUI>();
+                tmpPro.text = "YOU POWERED THE FACILITY!" +
+                    " THE GALACTIC FEDERATION THANKS YOU" +
+                    " FOR YOUR SERVICE";
+                tmpPro.color = Color.white;
+                StartCoroutine(ShowHint());
+            }
+        }
     }
 
     // Update is called once per frame
